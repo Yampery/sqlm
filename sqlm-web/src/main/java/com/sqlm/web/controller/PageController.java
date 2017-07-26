c4edb8573d5ff54fc4920eaa070fe5e0066de6f9
yampery <yampery@163.com>
2017-07-26 18:30:36 +0800

    初始框架

---
+++ sqlm-web/src/main/java/com/sqlm/web/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.sqlm.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面请求控制器
+ * @author Yampery
+ * @date 2017/7/26 16:12
+ */
+@Controller("pageController")
+public class PageController {
+
+    @RequestMapping("/{url}.do")
+    public String page(@PathVariable("url") String url) {
+        String page = url;
+        return page;
+    }
+}
---
+++ sqlm-web/src/main/java/com/sqlm/web/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Yampery
+ * @date 2017/7/26 16:05
+ */
+package com.sqlm.web;
